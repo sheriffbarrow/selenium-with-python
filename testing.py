3d97d185969e3c8e3f81e25690a1a08d6dab660f
¨sheriffbarrow¨ <smsbarrowjnr@gmail.com>
2022-12-19 14:50:09 +0000

    added checks for cookies

--- testing.py
+++ testing.py
@@ -5,17 +5,20 @@ import time
 
 url = 'https://www.python.org'
 
+def demo():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get(url)
+    print(len(driver.get_cookies()))
+    print( driver.get_cookies())
+    emaildd = driver.find_element(by=By.NAME, value='q')
 
-driver = webdriver.Chrome()
-driver.get(url)
+    button = driver.find_element(by=By.CSS_SELECTOR, value='button')
 
-emaildd = driver.find_element(by=By.NAME, value='q')
+    emaildd.send_keys('functions')
+    button.click()
+    print(driver.title)
 
-button = driver.find_element(by=By.CSS_SELECTOR, value='button')
-
-emaildd.send_keys('functions')
-button.click()
-print(driver.title)
-time.sleep(20)
+    time.sleep(20)
 
 
