332e81cb38363c06662a958f1557524b3bc8871d
¨sheriffbarrow¨ <smsbarrowjnr@gmail.com>
2022-12-21 11:04:31 +0000

    added evironment variable

--- facebook.py
+++ facebook.py
@@ -1,18 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-import time
+import time, os
 
 driver = webdriver.Chrome()
 url = 'https://web.facebook.com/?_rdc=1&_rdr'
 
 driver.get(url)
 
-user_name = driver.find_element(By.XPATH, value="//input[@id='email']")
-password = driver.find_element(By.XPATH, value="//input[@id='pass']")
-submit = driver.find_element(By.XPATH, value='//*[@id="u_0_9_p0"]')
+driver.implicitly_wait(10)
+user_name = driver.find_element(By.XPATH, value='//*[@id="email"]')
+password = driver.find_element(By.XPATH, value='//*[@id="pass"]')
+submit = driver.find_element(By.NAME, value='login')
 
-user_name.send_keys("s.hygienic@yahoo.com")
-password.send_keys('!s0249612579!SS')
+user_name.send_keys(os.environ.get('EMAIL'))
+password.send_keys(os.environ.get('PASSWORD'))
 submit.click()
+
 time.sleep(30)
