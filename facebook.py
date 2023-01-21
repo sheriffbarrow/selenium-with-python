271383dff3cbe252831283da7d400ecdde6f6554
¨sheriffbarrow¨ <smsbarrowjnr@gmail.com>
2023-01-21 23:40:43 +0000

    use pickle to save cookies for re-use

--- facebook.py
+++ facebook.py
@@ -3,19 +3,47 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 import os
+import pickle
 
-driver = webdriver.Chrome()
-url = 'https://web.facebook.com/?_rdc=1&_rdr'
+url = 'https://web.facebook.com'
 
-driver.get(url)
 
-driver.implicitly_wait(10)
-user_name = driver.find_element(By.XPATH, value='//*[@id="email"]')
-password = driver.find_element(By.XPATH, value='//*[@id="pass"]')
-submit = driver.find_element(By.NAME, value='login')
+def login():
 
-user_name.send_keys(os.environ.get('EMAIL'))
-password.send_keys(os.environ.get('PASSWORD'))
-submit.click()
+    driver = webdriver.Chrome()
+    driver.get(url)
+    driver.maximize_window()
 
-time.sleep(30)
+    driver.implicitly_wait(10)
+
+    user_name = driver.find_element(By.XPATH, value='//*[@id="email"]')
+    password = driver.find_element(By.XPATH, value='//*[@id="pass"]')
+    login = driver.find_element(By.NAME, value='login')
+
+    user_name.send_keys(os.environ.get('EMAIL'))
+    password.send_keys(os.environ.get('PASSWORD'))
+    login.click()
+    time.sleep(10)
+    cookies = driver.get_cookies()
+    pickle.dump(cookies, open('facebook.pkl', 'wb'))
+
+    time.sleep(5)
+
+
+def load_cookies():
+    driver = webdriver.Chrome()
+    cookie_list = pickle.load(open('facebook.pkl', "rb"))
+    url = 'https://web.facebook.com'
+    driver.get(url)
+    for cookie in cookie_list:
+        cookie['domain'] = '.facebook.com'
+        try:
+            driver.add_cookie(cookie)
+        except Exception as e:
+            print(e)
+    driver.get(url)
+    time.sleep(20)
+
+
+login()
+load_cookies()
