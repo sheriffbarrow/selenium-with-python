e93fe6abf30e31a7a9bcf49ce11baef49099f574
¨sheriffbarrow¨ <smsbarrowjnr@gmail.com>
2023-01-22 00:14:33 +0000

    used page object model with unittest

---
+++ PAGE_OBJECT_MODEL/locators/locators.py
@@ -0,0 +1,18 @@
+class LoginLocator:
+    locator_email = '// *[ @ id = "email"]'
+    locator_password = '// *[ @ id = "pass"]'
+    locator_button = 'login'
+
+
+class ProfilePage:
+    profiles = "//span[text()='Sheriff Barrow Jnr']"
+
+
+class LocatorBenefit:
+    cookie1 = '//*[@id="ccc-notify-accept"]'
+    benefit1 = "//button[@title='Benefits']"
+    uni_credit = '//*[@id="dekstopSubmenuModal"]/li[1]/div/div/div[1]/div[1]/div/div[1]/div[1]/a/span[3]'
+    ben_children = '//*[@id="dekstopSubmenuModal"]/li[1]/div/div/div[1]/div[1]/div/div[1]/div[2]/a/span[3]'
+    ben_sick = '//*[@id="dekstopSubmenuModal"]/li[1]/div/div/div[1]/div[1]/div/div[1]/div[3]/a/span[3]'
+    ben_later_life = '//*[@id="dekstopSubmenuModal"]/li[1]/div/div/div'
+
